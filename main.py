3b275c87c8bf1e9179236797918dc0ea15f3bef2
MemeDev2526 <memedev2526@outlook.com>
2025-10-03 10:27:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ app.add_middleware(
         "https://trickortreatsol.tech",
         "https://memedev2526.github.io",
         "https://memedev2526.github.io/-TREATZ",
+        "https://treatz-de1d.onrender.com",
         "http://localhost:5173",
         "http://127.0.0.1:8000",
         "http://localhost:8000",
